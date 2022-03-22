bc633ce65f4aa7c00a8213c26d781c1f0ea7ac5c
Al haddaoui Marwane <100091401+MarwaneAlh@users.noreply.github.com>
2022-03-22 21:45:00 +0100

    Ecran d'acceuil regler

--- Application/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Application/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,22 +7,15 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-    //Timer pour l'ecran d'acceuil
-    private static int SPLASH_TIME_OUT=4000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //code clean
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Gestion d'activité pour l'ecran d'acceuil
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent homeIntent = new Intent(MainActivity.this,HomeActivity.class);
-                startActivity(homeIntent);
-                finish();
-            }
-        },SPLASH_TIME_OUT);
+        //Creation de l'ecran splash
+
 
     }
 }
---
+++ Application/app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    //Cette class represente L'acticité splash qui s'affiche un court instant au lancement
+    // de l'application
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //Creation d'un thread qui va se lancer 3 milli secondes
+        //Gestions des erreurs ici gérer
+        //Apelle de la fonction startActivity afin de gerer la transition entre les deux activités
+        //Lancement du thread
+        Thread thread= new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(3000);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                }
+            }
+        };
+        thread.start();
+    }
+}
