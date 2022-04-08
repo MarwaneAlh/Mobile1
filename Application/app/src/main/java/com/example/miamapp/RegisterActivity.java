ba04be7dc0cad55d83fc5b656ed98068ffca8b20
Al haddaoui Marwane <100091401+MarwaneAlh@users.noreply.github.com>
2022-04-08 17:41:02 +0200

    Enregistrement de data dans firebase
    Enregistrement de data dans le cloud firestore modification des RegisterActivity ligne 91-99 plus ajout de firebase cloud au projet

--- Application/app/src/main/java/com/example/miamapp/RegisterActivity.java
+++ Application/app/src/main/java/com/example/miamapp/RegisterActivity.java
@@ -1,11 +1,15 @@
 package com.example.miamapp;
 
+
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -14,9 +18,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 //Class qui s'occupe de l'interface inscription
 
@@ -25,6 +37,10 @@ public class RegisterActivity extends AppCompatActivity {
     Button mregisterbtn;
     TextView mLoginBtn;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore ;
+    String userID;
+
+
 
 //Correspondance avec les elements xml du layout
     @Override
@@ -39,6 +55,8 @@ public class RegisterActivity extends AppCompatActivity {
         mregisterbtn =findViewById(R.id.connexionBtn);
         mLoginBtn =findViewById(R.id.connexiontext);
         fAuth = FirebaseAuth.getInstance();
+        fStore=FirebaseFirestore.getInstance();
+
 
 
         //Gestion de l'action lorsque l'utilisateur clique sur le bouton inscirption
@@ -48,6 +66,7 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
+                String fullName = mFullName.getText().toString().trim();
                 if(TextUtils.isEmpty(email)){
                     mEmail.setError("Veuillez entrer un email");
                     return;
@@ -67,6 +86,21 @@ public class RegisterActivity extends AppCompatActivity {
                         if(task.isSuccessful()){
                             Toast.makeText(RegisterActivity.this, "Compte créer !",
                                     Toast.LENGTH_SHORT).show();
+
+                        userID=fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference=fStore.collection("users").document(userID);
+                            Map<String,Object> user= new HashMap<>();
+                            user.put("fullName",fullName);
+                            user.put("email",email);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.d(TAG,"Data stored !");
+
+                                }
+                            });
+
+
                             startActivity(new Intent(getApplicationContext(),MainActivity.class));
                         }else{
                             Toast.makeText(RegisterActivity.this, "Erreur !"
